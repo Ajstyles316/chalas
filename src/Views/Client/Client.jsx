0e2d72bdb77ea6211f2f2d6a269e904082bbd986
hunk12369 <kevinbryanmora2@hotmail.com>
2024-12-01 16:05:56 -0400

    feat:respnsive and chathelper

--- src/Views/Client/Client.jsx
+++ src/Views/Client/Client.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import ProductGallery from './components/ProductGallery';
 import { algoliasearch } from 'algoliasearch';
@@ -6,12 +6,14 @@ import { InstantSearch } from 'react-instantsearch';
 import FeaturedAds from './components/FeaturedAds';
 import Footer from './components/Footer';
 import Chatbot from './components/Chatbot';
+import lamaLogo from '../../assets/img/llama_stupid.png'
 
 const APPLICATION_ID = 'DSW1VSW9MM';
 const SEARCH_API_KEY = 'f23cb9d50b6a40127c3a77ee34d81775';
 const ALGOLIA_INDEX = 'products';
 
 const client = algoliasearch(APPLICATION_ID, SEARCH_API_KEY);
+
 /*
 const searchClient = {
   ...client,
@@ -57,6 +59,15 @@ const searchClient = {
   },
 };
 const Client = () => {
+  const [isMessageVisible, setMessageVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessageVisible(false);
+    }, 9000); // El mensaje desaparece después de 5 segundos
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className="min-h-screen bg-gray-50 w-full">
       <InstantSearch searchClient={searchClient} indexName={ALGOLIA_INDEX}>
@@ -67,6 +78,20 @@ const Client = () => {
         </main>
         <Footer />
         <Chatbot />
+        {isMessageVisible && (
+          <div className="fixed bottom-4 right-4">
+            <div className="relative w-32 h-32">
+              <img
+                src={lamaLogo}
+                alt="Llama Chatbot"
+                className="w-full h-full object-contain rounded-full shadow-lg"
+              />
+              <div className="absolute bottom-32 left-0 transform -translate-x-1/2 w-36 bg-gray-900 text-white p-2 rounded-lg shadow-md">
+                <p>¡Hola! Soy tu asistente virtual</p>
+              </div>
+            </div>
+          </div>
+        )}
       </InstantSearch>
     </div>
   );
--- src/Views/Client/components/Header.jsx
+++ src/Views/Client/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { ShoppingCart, Percent, User, Search, MessageCircleDashed, FilterIcon } from 'lucide-react';
+import { ShoppingCart, Percent, User, Search, MessageCircleDashed, FilterIcon, MenuIcon } from 'lucide-react';
 import { Hits, Menu, SearchBox } from 'react-instantsearch';
 import lamaLogo from '../../../assets/img/appLogo.jpeg'
 import { FiUser, FiLogOut } from 'react-icons/fi';
@@ -9,18 +9,6 @@ import { useAuth } from '../../../context/AuthContext';
 import UnauthorizedView from '../../Users Module/view/UnauthorizedView';
 import DiscountCodes from '../../Transacciones/componentes/DiscountCodes';
 
-function Hit({ hit }) {
-  return (
-    <div className="space-y-4">
-      <div className="flex items-center space-x-4 border rounded-lg p-3 hover:shadow-md transition bg-white-50">
-        <div className="flex-1">
-          <h3 className="font-bold text-gray-600">{hit.name_product}</h3>
-          <p className="text-sm text-gray-500">{hit.description}</p>
-        </div>
-      </div>
-    </div>
-  );
-}
 const Header = () => {
 
   // const navigate = useNavigate();
@@ -28,12 +16,16 @@ const Header = () => {
 
   const { user } = useAuth();
 
-  console.log({ user });
-  const [isDiscountCodesVisible, setIsDiscountCodesVisible] = useState(false); 
+  //console.log({ user });
+  const [isDiscountCodesVisible, setIsDiscountCodesVisible] = useState(false);
+  const [isFocused, setIsFocused] = useState(true);
   const [hasText, setHasText] = useState(false);
   const [showHits, setShowHits] = useState(false); // Estado para controlar si los hits se muestran
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuFilterOpenPhone, setIsMenuFilterOpenPhone] = useState(false);
+  const [isMenuOpenPhone, setIsMenuOpenPhone] = useState(false);
   const [isMenuClientOpen, setIsMenuClientOpen] = useState(false);
+  const [searchText, setSearchText] = useState(""); // El texto que el usuario escribe
   const [isLogoutConfirmationVisible, setIsLogoutConfirmationVisible] = useState(false);
   const [isProfileFormVisible, setIsProfileFormVisible] = useState(false);
   const menuRef = useRef(null);
@@ -50,6 +42,12 @@ const Header = () => {
   const toggleMenu = () => {
     setIsMenuOpen((prevState) => !prevState);
   };
+  const toggleMenuPhone = () => {
+    setIsMenuOpenPhone((prevState) => !prevState); // Cambiar el estado del menú
+  };
+  const toggleMenuFilterPhone = () => {
+    setIsMenuFilterOpenPhone((prevState) => !prevState); // Cambiar el estado del menú
+  };
 
   const toggleMenuClient = () => {
     setIsMenuClientOpen((prevState) => !prevState)
@@ -60,6 +58,7 @@ const Header = () => {
   const handleInputChange = (event) => {
     setShowHits(true); // Oculta los hits al presionar Enter
     setHasText(event.target.value.trim().length > 0);
+    //setSearchText(event.target.value);
   };
   const handleKeyDown = (e) => {
     if (e.key === 'Enter') {
@@ -69,6 +68,9 @@ const Header = () => {
 
   const handleBlur = () => {
     setShowHits(false); // Oculta los hits cuando se hace clic fuera del SearchBox
+  };
+  const handleFocus = () => {
+    setIsFocused(false); // El campo está enfocado
   };
 
   const confirmLogout = async () => {
@@ -102,6 +104,27 @@ const Header = () => {
     setIsMenuClientOpen(false); // Cierra el menú al abrir el formulario
   };
 
+  const handleSelectSuggestion = (suggestion) => {
+    setSearchText(suggestion);  // Completar el texto con la sugerencia seleccionada
+    //console.log(searchText);
+    setShowHits(false); // Oculta los hits cuando se hace clic fuera del SearchBox
+    setIsFocused(true); // El campo está enfocado
+    //setShowHitsearchTexts(false);  // Ocultar los hits
+  };
+  function Hit({ hit }) {
+    return (
+      <div className="space-y-4" onClick={() => handleSelectSuggestion(hit.name_product)}>
+        <div className="flex items-center space-x-4 border rounded-lg p-3 hover:shadow-md transition bg-white-50">
+          <div className="flex-1">
+            <h3 className="font-bold text-gray-600">{hit.name_product}</h3>
+            <p className="text-sm text-gray-500">{hit.description}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
@@ -113,14 +136,14 @@ const Header = () => {
   if (user != null) {
     return (
       <header className="border-b border-gray-200 bg-white sticky top-0 z-50">
-        <div className="max-w-[1920px] mx-auto px-6 py-4">
+        <div className="max-w-screen-xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between">
             <div className="flex items-center gap-4 ">
               <div className="w-32 h-32">
                 <img
                   src={lamaLogo}
                   alt="ChalitaOE Logo"
-                  className="w-full h-full"
+                  className="w-full h-full object-contain"
                 />
               </div>
               <div className="flex flex-col">
@@ -129,8 +152,8 @@ const Header = () => {
               </div>
             </div>
 
-            <div className="flex-1 max-w-2xl mx-8">
-              <div className="relative">
+            <div className="hidden md:flex max-w-2xl mx-8">
+              <div className="relative" >
                 <SearchBox
                   placeholder="Buscar productos..."
                   classNames={{
@@ -146,8 +169,13 @@ const Header = () => {
                   }}
                   onInput={handleInputChange}
                   onKeyDown={handleKeyDown}
-                  onBlur={handleBlur}
+                  onFocus={handleFocus}
                 />
+                {isFocused && (
+                  <div className="absolute top-0 left-0 w-full h-full flex items-center pl-4 pointer-events-none bg-white rounded-lg border border-gray-300">
+                    {searchText}
+                  </div>
+                )}
                 <div className="absolute top-full left-0 w-full bg-white border border-gray-300 rounded-lg shadow-lg z-10">
                   {showHits && <Hits hitComponent={Hit} />}
                 </div>
@@ -155,19 +183,25 @@ const Header = () => {
               </div>
             </div>
 
-            <div className="flex items-center gap-6">
+            <div className="md:hidden">
+              <button className="text-gray-600 focus:outline-none" onClick={toggleMenuPhone}>
+                <MenuIcon className="w-7 h-6" />
+              </button>
+            </div>
+
+            <div className="hidden md:flex space-x-6 items-center gap-6">
 
               <button className="flex flex-col items-center text-gray-600 hover:text-[#8B0000] transition-colors" onClick={handleAddToCart}>
                 <ShoppingCart className="w-6 h-6" />
                 <span className="text-xs mt-1">Carrito</span>
               </button>
               <button
-              className="flex flex-col items-center text-gray-600 hover:text-[#8B0000] transition-colors"
-              onClick={toggleDiscountCodes}
-            >
-              <Percent className="w-6 h-6" />
-              <span className="text-xs mt-1">Descuentos</span>
-            </button>
+                className="flex flex-col items-center text-gray-600 hover:text-[#8B0000] transition-colors"
+                onClick={toggleDiscountCodes}
+              >
+                <Percent className="w-6 h-6" />
+                <span className="text-xs mt-1">Descuentos</span>
+              </button>
               <div className="relative">
                 <button onClick={toggleMenu} className="flex flex-col items-center text-gray-600 hover:text-[#8B0000] transition-colors">
                   <FilterIcon className="w-6 h-6" />
@@ -228,12 +262,60 @@ const Header = () => {
               </div>
             </div>
           </div>
+          {isMenuOpenPhone && (
+            <div className="md:hidden mt-4 space-y-4">
+              <div className="relative" >
+                <SearchBox
+                  placeholder="Buscar productos..."
+                  classNames={{
+                    input: "w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-transparent",
+                    //submitIcon: "pl-8 pr-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:border-gray-500",
+                    //submitIcon: "h-8 w-8 absolute right-2 top-2 bg-orange-500 text-white p-2 rounded-lg hover:bg-orange-600 transition",
+                    submitIcon: `h-8 w-8 absolute right-1 top-2 bg-white-500 text-white p-2 rounded-lg hover:bg-gray-600 transition ${hasText ? "hidden" : ""
+                      }`,
+                    resetIcon: `h-8 w-8 absolute right-1 top-2 bg-white-500 text-white p-2 rounded-lg hover:bg-gray-600 transition ${hasText ? "hidden" : ""
+                      }`,
+                    loadingIcon: "h-8 w-8 absolute right-2 top-2 bg-white-500 text-white p-2 rounded-lg hover:bg-gray-600 transition",
+
+                  }}
+                  onInput={handleInputChange}
+                  onKeyDown={handleKeyDown}
+                  onFocus={handleFocus}
+                />
+                {isFocused && (
+                  <div className="absolute top-0 left-0 w-full h-full flex items-center pl-4 pointer-events-none bg-white rounded-lg border border-gray-300">
+                    {searchText}
+                  </div>
+                )}
+                <div className="absolute top-full left-0 w-full bg-white border border-gray-300 rounded-lg shadow-lg z-10">
+                  {showHits && <Hits hitComponent={Hit} />}
+                </div>
+
+              </div>
+              <span className="block text-gray-600 hover:text-gray-900">Carrito</span>
+              <span className="block text-gray-600 hover:text-gray-900">Descuentos</span>
+              <button onClick={toggleMenuFilterPhone}>
+                <span className="block text-gray-600 hover:text-gray-900">Filtros</span>
+              </button>
+              {isMenuFilterOpenPhone && (
+                <div ref={menuRef} >
+                  <Menu attribute="categories" showMoreLimit={25}
+                    classNames={{
+                      selectedItem: "font-bold text-gray-900",
+                      item: "block text-gray-600 hover:text-gray-900",
+                    }}
+                  />
+                </div>
+              )}
+
+            </div>
+          )}
         </div>
         {isDiscountCodesVisible && (
-        <div className="absolute top-20 right-4 w-100 h-96 bg-white border border-gray-300 rounded-lg shadow-lg overflow-y-auto z-20 p-4">
-          <DiscountCodes />
-        </div>
-      )}
+          <div className="absolute top-20 right-4 w-100 h-96 bg-white border border-gray-300 rounded-lg shadow-lg overflow-y-auto z-20 p-4">
+            <DiscountCodes />
+          </div>
+        )}
       </header>
 
     );
